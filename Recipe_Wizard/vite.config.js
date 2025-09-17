d1a796dab65c05e6859d37ebbbf9f5a93667ae97
Grant Williams <grantwilliams@oakland.edu>
2025-09-16 20:11:58 -0400

    Set base path for Vite configuration

--- Recipe_Wizard/vite.config.js
+++ Recipe_Wizard/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// replace REPO_NAME with your repo name (case-sensitive)
 export default defineConfig({
   plugins: [react()],
+  base: '/Recipe_Wizard/',
 })
