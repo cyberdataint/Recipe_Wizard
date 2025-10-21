3a96613530dadb2b78e0576337c9428c60523d29
Grant Williams <116682738+cyberdataint@users.noreply.github.com>
2025-10-20 21:01:59 -0400

    netfily updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Use root path for Netlify deployment
 export default defineConfig({
   plugins: [react()],
-
+  base: '/',
 })
